b98dc4fd19525abf3ceb2872130d786adf9a2e9a
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-12-17 14:45:38 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+
+class PerguntaApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    final perguntas = [
+      ' Qual é a sua cor favorita?',
+      ' Qual é o seu animal favorito?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Perguntas"),
+        ),
+        body: Column(
+          children:  [
+            Text(perguntas[0]),
+            const ElevatedButton(onPressed: null,
+                child: Text('Resposta 1')
+            ),
+            const ElevatedButton(onPressed: null,
+                child: Text('Resposta 2')
+            ),
+            const ElevatedButton(onPressed: null,
+                child: Text('Resposta 3')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
